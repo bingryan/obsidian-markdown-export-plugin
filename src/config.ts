bee2789a7c86ae3aa38936ade09abc525907143e
bingryan <41174435+bingryan@users.noreply.github.com>
2022-11-16 00:09:15 +0800

    add GitHub Flavored Markdown option (#3)

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,15 @@
 export const IMAGE_URL_REGEXP = /!\[\[(.*?)\]\]/g;
 
+export const GMT_IMAGE_FORMAT = "![]({0})";
+
 export interface MarkdownExportPluginSettings {
 	output: string;
 	attachment: string;
+	GTM: boolean;
 }
 
 export const DEFAULT_SETTINGS: MarkdownExportPluginSettings = {
 	output: "output",
 	attachment: "attachment",
+	GTM: true,
 };
