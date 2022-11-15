a14c70a6ab837760a89d53a8948659042c8d0f2b
bingryan <bingryan@outlook.com>
2022-11-15 22:44:17 +0800

    add export single markdown logic

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,11 @@
+export const IMAGE_URL_REGEXP = /!\[\[(.*?)\]\]/g;
 
 export interface MarkdownExportPluginSettings {
 	output: string;
+	attachment: string;
 }
 
 export const DEFAULT_SETTINGS: MarkdownExportPluginSettings = {
-	output: 'default'
-}
+	output: "output",
+	attachment: "attachment",
+};
--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,74 @@
-import { App, Editor, MarkdownView, Modal, MenuItem,Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-import { MarkdownExportPluginSettings,DEFAULT_SETTINGS } from './config';
+import {
+	App,
+	MenuItem,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
+import { MarkdownExportPluginSettings, DEFAULT_SETTINGS } from "./config";
 
+import * as path from "path";
+import { tryCreateFolder, tryCopyImage, tryReadMarkdown } from "./utils";
 
 export default class MarkdownExportPlugin extends Plugin {
 	settings: MarkdownExportPluginSettings;
 
 	async onload() {
-		
 		await this.loadSettings();
-
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new MarkdownExportSettingTab(this.app, this));
 
-
 		this.registerEvent(
-			this.app.workspace.on('file-menu', (menu, file) => {
-			  const addMenuItem = (item: MenuItem) => {
-				item.setTitle('Export all to package');
-				item.onClick(() => {
-					// TODO: file export logic
-					new Notice('This is a notice!');
-				});
-			  };
-	  
+			this.app.workspace.on("file-menu", (menu, file) => {
+				const addMenuItem = (item: MenuItem) => {
+					item.setTitle("Export all to package");
+					item.onClick(async () => {
+						// try create output directory
+						await tryCreateFolder(this, this.settings.output);
+						// try create attachment directory
+						await tryCreateFolder(
+							this,
+							path.join(
+								this.settings.output,
+								this.settings.attachment
+							)
+						);
 
-			  menu.addItem(addMenuItem);
+						// copy markdown file to output directory
+						await tryReadMarkdown(this, file.path, file.name);
 
-			}),
-		  );
-	
+						// copy image to attachment directory
+						await tryCopyImage(this, file.path);
+
+						new Notice(
+							`Exporting ${file.path} to ${path.join(
+								this.settings.output,
+								file.name
+							)}`
+						);
+					});
+				};
+				menu.addItem(addMenuItem);
+			})
+		);
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
 }
-
 
 class MarkdownExportSettingTab extends PluginSettingTab {
 	plugin: MarkdownExportPlugin;
@@ -55,22 +79,38 @@ class MarkdownExportSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Markdown Export'});
+		containerEl.createEl("h2", { text: "Markdown Export" });
 
 		new Setting(containerEl)
-			.setName('Custom default output path')
-			.setDesc('default directory for one-click export')
-			.addText(text => text
-				.setPlaceholder('Enter default output path')
-				.setValue(this.plugin.settings.output)
-				.onChange(async (value) => {
-					console.log('output path: ' + value);
-					this.plugin.settings.output = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Custom default output path")
+			.setDesc("default directory for one-click export")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter default output path")
+					.setValue(this.plugin.settings.output)
+					.onChange(async (value) => {
+						console.log("output path: " + value);
+						this.plugin.settings.output = value;
+						await this.plugin.saveSettings();
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("Custom attachment path(optional)")
+			.setDesc("attachment path")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter attachment path")
+					.setValue(this.plugin.settings.attachment)
+					.onChange(async (value) => {
+						console.log("attachment path: " + value);
+						this.plugin.settings.output = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
