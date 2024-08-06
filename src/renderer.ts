72bc351c43ae4a3e737d48a693a8cc607ada5fb0
bingryan <41174435+bingryan@users.noreply.github.com>
2024-08-06 22:54:43 +0800

    fix re groups (#92)
    * fix re groups

--- src/renderer.ts
+++ src/renderer.ts
@@ -3,7 +3,9 @@ import { MarkdownRenderer, MarkdownView } from 'obsidian';
 import MarkdownExportPlugin from "./main";
 
 export default async function markdownToHTML(plugin: MarkdownExportPlugin, inputFile: string, inputContent: string) {
+	// @ts-ignore
 	let activeView = app.workspace.getActiveViewOfType(MarkdownView);
+	// @ts-ignore
 	const leaf = app.workspace.getLeaf(true);
 	if (!activeView) {
 		activeView = new MarkdownView(leaf);
